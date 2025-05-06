99af7c560888e0b2c85c7ea0be62d66928e91782
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-05-06 10:10:22 +0900

    Time: 532 ms (29.57%), Space: 166 MB (59.16%) - LeetHub

---
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findCommonResponse(self, responses: List[List[str]]) -> str:
+        cnt = Counter()
+        for response in responses:
+            uniq = set(response)
+            for word in uniq:
+                cnt[word] += 1
+        
+        mx = 0
+        result = None
+        for k, v in cnt.items():
+            if v > mx:
+                mx = v
+                result = k
+            elif v == mx:
+                result = min(result, k) if result else k
+        
+        return result
+                
