5ef96709a04d33cae938302c6e47aa6c0b82356b
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-06-23 21:06:42 +0900

    Time: 50 ms (20.7%), Space: 55.8 MB (44.27%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.ts
@@ -0,0 +1,10 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    if(obj instanceof Array) {
+        return obj.length === 0;
+    } else {
+        return Object.keys(obj).length === 0;
+    }
+};
