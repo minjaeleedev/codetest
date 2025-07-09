26a4d42177d052da3e769d1903d8945c4bad2d92
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-07-09 15:01:16 +0900

    Time: 7 ms (77.09%), Space: 18 MB (86.69%) - LeetHub

---
+++ 3606-coupon-code-validator/3606-coupon-code-validator.py
@@ -0,0 +1,26 @@
+import re
+class Solution:
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        n = len(code)
+        category = set(["electronics", "grocery", "pharmacy", "restaurant"])
+        result = []
+        for i in range(n):
+            c, bl, a = code[i], businessLine[i], isActive[i]
+            if not c:
+                continue
+
+            if not re.match(r'^[a-zA-z0-9_]+$', c):
+                continue
+            
+            if bl not in category:
+                continue
+
+            if not a:
+                continue
+
+            obj = {'code':c, 'businessLine':bl, 'isActive':a}
+            result.append(obj)
+
+        result.sort(key=lambda x:(x['businessLine'], x['code']))
+
+        return [obj['code'] for obj in result]
