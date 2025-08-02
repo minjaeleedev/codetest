6cefb756865b740188d5987188f05759cf475921
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-08-02 23:45:12 +0900

    Time: 3 ms (45.43%), Space: 18.1 MB (5.31%) - LeetHub

---
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.py
@@ -0,0 +1,15 @@
+class Solution:
+    def distMoney(self, money: int, children: int) -> int:
+        # distribute to all children
+        money -= children
+        if money < 0:
+            return -1
+        
+        if money // 7 == children and money % 7 == 0:
+            # we can distribute same amount to all children
+            return children
+        
+        if money // 7 == children - 1 and money % 7 == 3:
+            return children - 2
+        
+        return min(children-1, money//7)
