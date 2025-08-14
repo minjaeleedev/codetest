6f3e1533c6a66655b18e79820016b27b2db0de2c
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-08-15 06:53:55 +0900

    Time: 0 ms (100%), Space: 19 MB (97.55%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -3,15 +3,7 @@ class Solution:
         ones = 0
         twos = 0
         for num in nums:
-            # Update twos: add bits that were in ones and now appear again
-            twos = twos | (ones & num)
-            
-            # Update ones: toggle bits with XOR
-            ones = ones ^ num
-            
-            # Clear bits that appear three times (present in both ones and twos)
-            threes = ones & twos
-            ones = ones & ~threes
-            twos = twos & ~threes
-        
+            ones ^= (num & ~twos)
+            twos ^= (num & ~ones)
+
         return ones
