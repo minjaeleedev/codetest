8b2809efaf543a492ad5e039d713d91fe5c7d15c
minjae lee <77224652+minjaeleedev@users.noreply.github.com>
2025-09-24 19:37:10 +0900

    Time: 52 ms (52.57%), Space: 18.9 MB (97.7%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node-bfs.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        q = deque([])
+        if root:
+            q.append(root)
+            
+        while q:
+            l = len(q)
+            for i in range(l):
+                cur = q.popleft()
+                if i != l-1:
+                    cur.next = q[0]
+                if cur.left:
+                    q.append(cur.left)
+                if cur.right:
+                    q.append(cur.right)
+        
+        
+        return root
