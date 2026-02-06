9f0c4336832fe5401f4833f089ff0137bd876927
Miles Lee <miles.lee@buzzvil.com>
2026-02-06 23:11:29 +0900

    leetcode medium 3634

---
+++ LeetCode/3634-minimum-removals-to-balance-array/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        nums.sort()
+
+        ans = n
+        right = 0
+        for left in range(n):
+            while right < n and nums[right] <= nums[left] * k:
+                right += 1
+            ans = min(ans, n - (right - left))
+
+        return ans
